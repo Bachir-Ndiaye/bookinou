a633f4427fb5aa4fba58c69ee68724ac56a99c79
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2021-06-24 13:27:39 +0200

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const axios = require('axios');
+  const [data, setData] = useState(null);
+
+  const showData = () => {
+    //Authenticate to the API to get the Bearer Token
+    const loginResponse = axios.post('https://api-v2.7-dev.mybookinou.com/api/users/login',
+      {
+        "username": "technical_test@bookinou.com",
+        "password": "test69"
+      })
+      .then((res) => res.data)
+      .then((data) => data["hydra:description"])
+      .then((token) => {
+        console.log(`Bearer ${token}`);
+        axios.post('https://api-v2.7-dev.mybookinou.com/api/graphql',
+          {
+            headers: { Authorization: `Bearer ${token}` },
+            query: `
+                {
+                  stories {
+                    edges {
+                      node {
+                        id
+                        title
+                        picture {
+                          id
+                          contentUrl
+                                }
+                          }
+                          }
+                          }
+                }
+                `
+          }
+        )
+          .then((res) => setData(res.data))
+          .then((datas) => console.log(datas))
+          .catch((err) => console.log(err));
+      });
+}
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <Button variant="primary" onClick={showData}>Show Data</Button>
+    </header>
+  </div>
+);
 }
 
 export default App;
